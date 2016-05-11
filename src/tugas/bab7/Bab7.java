a0f07ddb379fd887de8da6605cf2e858ecdfd580
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-11 20:36:29 +0700

    edit main class

---
+++ src/tugas/bab7/Bab7.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polymorfisme;
+/**
+ *
+ * @author Prasetyo
+ */
+public class Bab7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Silver silver = new Silver();
+    Gold gold = new Gold();
+    Premium premium = new Premium();
+//    Pelanggan[] pelanggan = new Pelanggan[3];
+//    silver.pelangan(10);
+//    pelanggan[1]= silver;
+//    pelanggan[2]= premium;
+//    pelanggan[3]= gold;
+//    
+    silver.DaftarProduk();
+    }
+    
+}
