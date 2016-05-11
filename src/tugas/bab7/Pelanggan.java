7b95121edd15f07027333e624128088d63488adb
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-11 20:37:02 +0700

    edit method class pelanggan

---
+++ src/tugas/bab7/Pelanggan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polymorfisme;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Prasetyo
+ */
+public abstract class Pelanggan {
+
+    int urutan;
+    int hasil;
+    int tol;
+    int diskon;
+    String pilih;
+    Scanner in = new Scanner(System.in);
+    public Pelanggan(){
+        
+    }
+    
+    
+    
+
+    
+    }
