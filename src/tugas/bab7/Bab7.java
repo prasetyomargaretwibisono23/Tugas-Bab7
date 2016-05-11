1507915920ce56e34bf9764a0e5ca57047d9b6c6
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-11 22:14:35 +0700

    edit main class

--- src/tugas/bab7/Bab7.java
+++ src/tugas/bab7/Bab7.java
@@ -1,29 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Polymorfisme;
-/**
- *
- * @author Prasetyo
- */
+import java.util.Scanner;
 public class Bab7 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-    Silver silver = new Silver();
-    Gold gold = new Gold();
-    Premium premium = new Premium();
-//    Pelanggan[] pelanggan = new Pelanggan[3];
-//    silver.pelangan(10);
-//    pelanggan[1]= silver;
-//    pelanggan[2]= premium;
-//    pelanggan[3]= gold;
-//    
-    silver.DaftarProduk();
+    int urt;
+    Scanner si = new Scanner(System.in);
+    Silver silver = new Silver("nana","malang",1);
+    Gold gold = new Gold("riri","lawang",2);
+    Premium premium = new Premium("sendy","singosari",3);
+    Pelanggan[] pelanggan = new Pelanggan[4];
+    silver.pelangan(10);
+    pelanggan[1]= silver;
+    pelanggan[2]= premium;
+    pelanggan[3]= gold;
+    do{
+        System.out.println("Salon");
+        System.out.println("1.lihat daftar harga");
+        System.out.println("2.Pesan produk");
+        System.out.println("3.Cek Pesanan");
+        System.out.println("0.Keluar");
+        System.out.println("Masukkan urutan : ");
+        urt=si.nextInt();
+        switch(urt){
+        case 1 :
+            silver.daftar();break;
+        case 2 :
+            silver.DaftarProduk();break;
+        case 3 :
+            silver.cek();
+            premium.cek();
+            gold.cek();
     }
-    
+    }while(urt !=0);
+        System.out.println("Terima Kasih Sudah berkunjung Ke tempat kami");
+    }
 }
